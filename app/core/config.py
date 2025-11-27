bbc520f2cd8eeab5ca2033b5ec352816a78d1cf2
João Wesley D. Kind <joaodkind@gmail.com>
2025-11-27 19:12:27 -0300

    up

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str = "postgresql://user:password@localhost/mks_store"
