2b33db69517eaef23cba364b9ab670cfe8b61339
João Wesley D. Kind <joaodkind@gmail.com>
2025-12-09 19:24:59 -0300

    up

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python fastapi_production.py
--- fastapi_app.py
+++
@@ -1,5 +0,0 @@
-from fastapi_production import *
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
